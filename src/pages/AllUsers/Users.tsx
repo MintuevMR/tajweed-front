f141602c3cc19c75d8b012d5ee4f5c714c112ba4
ali <loading.03@mail.ru>
2023-07-26 11:27:03 +0300

    Сделал поиск пользователя по фамилии

--- src/pages/AllUsers/Users.tsx
+++ src/pages/AllUsers/Users.tsx
@@ -14,3 +14,6 @@ const Users = ({ user, loading }) => {
   );
 };
 export default Users;
+
+
+
--- src/pages/AllUsers/index.tsx
+++ src/pages/AllUsers/index.tsx
@@ -3,7 +3,6 @@ import ProfileSidebar from '../Profile/ProfileSidebar'
 import UsersFetch from './UsersFetch'
 import styles from './usersList.module.css'
 
-
 const index = () => {
   return (
 	 <div className={styles.main}>
