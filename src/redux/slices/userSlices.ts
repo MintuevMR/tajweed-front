4aff9fe046206ce1bdece89cdb8191393c71528e
Mintuev <mail@example.com>
2023-07-31 12:52:10 +0300

    типизация userlice

--- src/redux/slices/userSlices.ts
+++ src/redux/slices/userSlices.ts
@@ -1,4 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "../store/store";
+//import { ThunkAPI } from "@reduxjs/toolkit/dist/configureStore";
 
 import {
   handleUserInfoFulfilled,
@@ -13,6 +15,18 @@ import {
 interface UserState {
   user: UserItem;
   users: UserItem[];
+  bookmarks: IBookmarks[];
+  error: unknown;
+}
+
+interface IBookmarks {
+  _id: string;
+  name: string;
+  title: string;
+  description: string;
+  text: string;
+  ref: string;
+  __v: number;
 }
 
 interface UserItem {
@@ -22,18 +36,29 @@ interface UserItem {
   lastName: string;
   role: string;
   password: string;
+  avatar: string;
   __v: number;
 }
 
 const initialState: UserState = {
-  user: {},
+  user: {} as UserItem,
   users: [],
   bookmarks: [],
   error: null,
 };
 
+type AsyncThunkReturnType = UserItem;
+
 // Инфо пользователя
-export const userInfo = createAsyncThunk("user/Info", async (_, thunkAPI) => {
+export const userInfo = createAsyncThunk<
+  AsyncThunkReturnType,
+  void,
+  { state: RootState }
+>("user/Info", async (_, thunkAPI) => {
+  // const { application } = thunkAPI.getState() as {
+  //   application: { token: string };
+  // };
+
   try {
     const res = await fetch("http://localhost:3000/profile", {
       method: "GET",
@@ -50,69 +75,72 @@ export const userInfo = createAsyncThunk("user/Info", async (_, thunkAPI) => {
 });
 
 // Изменение пользователя
-export const userChangeInfo = createAsyncThunk(
-  "user/Change",
-  async ({ firstName, lastName }, thunkAPI) => {
-    try {
-      const res = await fetch("http://localhost:3000/profile", {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${thunkAPI.getState().application.token}`,
-        },
-        body: JSON.stringify({ firstName, lastName }),
-      });
-      const json = await res.json();
+export const userChangeInfo = createAsyncThunk<
+  AsyncThunkReturnType,
+  { firstName: string; lastName: string },
+  { state: RootState }
+>("user/Change", async ({ firstName, lastName }, thunkAPI) => {
+  try {
+    const res = await fetch("http://localhost:3000/profile", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${thunkAPI.getState().application.token}`,
+      },
+      body: JSON.stringify({ firstName, lastName }),
+    });
+    const json = await res.json();
 
-      if (json.error) {
-        return thunkAPI.rejectWithValue(json.error);
-      }
-
-      return json;
-    } catch (error) {
-      thunkAPI.rejectWithValue(error);
+    if (json.error) {
+      return thunkAPI.rejectWithValue(json.error);
     }
-  }
-);
 
-export const userChangeAvatar = createAsyncThunk(
-  "user/ChangeAvatar",
-  async (data, thunkAPI) => {
-    try {
-      const res = await fetch("http://localhost:3000/profile/change-img", {
-        method: "PATCH",
-        headers: {
-          Authorization: `Bearer ${thunkAPI.getState().application.token}`,
-        },
-        body: data,
-      });
-      const json = await res.json();
-      return json;
-    } catch (error) {
-      thunkAPI.rejectWithValue(error);
-    }
+    return json;
+  } catch (error) {
+    thunkAPI.rejectWithValue(error);
   }
-);
+});
 
-export const bookmark = createAsyncThunk(
-  "user/addBookmark",
-  async (moduleId, thunkAPI) => {
-    try {
-      const res = await fetch("http://localhost:3000/bookmark", {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${thunkAPI.getState().application.token}`,
-        },
-        body: JSON.stringify({ moduleId }),
-      });
-      const data = await res.json();
-      return data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
-    }
+export const userChangeAvatar = createAsyncThunk<
+  void,
+  FormData,
+  { state: RootState }
+>("user/ChangeAvatar", async (data: FormData, thunkAPI) => {
+  try {
+    const res = await fetch("http://localhost:3000/profile/change-img", {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${thunkAPI.getState().application.token}`,
+      },
+      body: data,
+    });
+    const json = await res.json();
+    return json;
+  } catch (error) {
+    thunkAPI.rejectWithValue(error);
   }
-);
+});
+
+export const bookmark = createAsyncThunk<
+  AsyncThunkReturnType,
+  void,
+  { state: RootState }
+>("user/addBookmark", async (moduleId, thunkAPI) => {
+  try {
+    const res = await fetch("http://localhost:3000/bookmark", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${thunkAPI.getState().application.token}`,
+      },
+      body: JSON.stringify({ moduleId }),
+    });
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
 
 export const userAll = createAsyncThunk("user/all", async (_, thunkAPI) => {
   try {
